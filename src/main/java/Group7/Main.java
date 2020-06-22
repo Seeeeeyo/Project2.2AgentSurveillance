ba35903875ecbfd2e6fd55bf2a86bca225c64cef
mihaitudor9 <43904961+mihaitudor9@users.noreply.github.com>
2020-06-22 20:45:28 +0300

    Reverting Mohammed's changes (Not freezing anymore) + Scaled up the map, also increasing the radius of agents in the process

--- src/main/java/Group7/Main.java
+++ src/main/java/Group7/Main.java
@@ -13,7 +13,7 @@ public class Main {
         double[] captureTimes = new double[nGames];
         int winCounter = 0;
         for (int i = 0; i < nGames; i++) {
-            Game game = new Game(Parser.parseFile("C:\\Users\\steph\\Documents\\GitHub\\Project2.2AgentSurveillance\\src\\main\\java\\Group7\\map\\maps\\generatedMap-SMALLEST.map"), new DefaultAgentFactory(), false);
+            Game game = new Game(Parser.parseFile("src/main/java/Group7/map/maps/generatedMap-SMALLEST.map"), new DefaultAgentFactory(), false);
 
             long start = System.currentTimeMillis();
             game.run();
@@ -21,10 +21,10 @@ public class Main {
             long finish = System.currentTimeMillis();
             long timeElapsed = finish - start;
             if (game.getWinner() == Game.Team.GUARDS) {
-                winCounter++;
                 nGamesWon++;
                 averageTime = averageTime + timeElapsed;
                 captureTimes[winCounter] = timeElapsed;
+                winCounter++;
             }
             //System.out.println("Game took " + timeElapsed / 1000 + " seconds to reach an outcome");
         }
